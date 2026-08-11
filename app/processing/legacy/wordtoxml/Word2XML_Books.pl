e7e3b7f2d1def919ae7c94cd3f4594f8aead5701
Murali Balu <Muraliba@S4CARLISLE.NET>
2026-08-11 15:07:26 +0530

    perl update

--- app/processing/legacy/wordtoxml/Word2XML_Books.pl
+++ app/processing/legacy/wordtoxml/Word2XML_Books.pl
@@ -469,9 +469,55 @@ sub DTDvalidate
 	print $LOG "---------------------------------\n";
 
 	# -------- XML Parser --------
+	my $script_dir = dirname(abs_path($0));
 	my $parser = XML::LibXML->new(
-	    load_ext_dtd => 1,
-	    validation   => 1
+	    load_ext_dtd    => 1,
+	    validation      => 1,
+	    expand_entities => 1,
+	    ext_ent_handler => sub {
+	        my ($system_id, $public_id) = @_;
+	        my $resolved_id = $system_id;
+	        
+	        # Map `/app/wordtoxml` to the actual script directory
+	        $resolved_id =~ s{^/app/wordtoxml}{$script_dir}g;
+	        
+	        # Extract filename (e.g., BITS-xinclude1.ent)
+	        my ($filename) = fileparse($resolved_id);
+	        
+	        # List of subdirectories to search in order
+	        my @search_dirs = (
+	            "$script_dir/BITS-Book-1.0-DTD",
+	            "$script_dir/BITS-Book-1.0-DTD/mathml",
+	            "$script_dir/BITS-Book-1.0-DTD/iso8879",
+	            "$script_dir/BITS-Book-1.0-DTD/iso9573-13",
+	            "$script_dir/BITS-Book-1.0-DTD/xmlchars"
+	        );
+	        
+	        my $found = 0;
+	        foreach my $s_dir (@search_dirs) {
+	            my $test_file = "$s_dir/$filename";
+	            if (-f $test_file) {
+	                $resolved_id = $test_file;
+	                $found = 1;
+	                last;
+	            }
+	        }
+	        
+	        # Otherwise, if not absolute, resolve relative to script_dir
+	        if (!$found && $resolved_id !~ m{^/} && $resolved_id !~ m{^[A-Za-z]:}) {
+	            $resolved_id = "$script_dir/$resolved_id";
+	        }
+	        
+	        if (-f $resolved_id) {
+	            open(my $fh, '<', $resolved_id) or die "Cannot open $resolved_id: $!";
+	            local $/ = undef;
+	            my $content = <$fh>;
+	            close $fh;
+	            return $content;
+	        }
+	        
+	        return "";
+	    }
 	);
 
 	eval {
